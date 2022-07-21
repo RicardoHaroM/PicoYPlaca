b44791c50d2f08264d5da1507ae168ee6b04475b
Ricardo Haro <sr.ricardoharo@gmail.com>
2022-07-21 00:56:04 -0500

    20220721 - update holiday test

--- test/dateManager.test.js
+++ test/dateManager.test.js
@@ -1,7 +1,7 @@
 const DateManager = require('../src/helpers/dateManager')
 
 test('Check if a holiday date is a holiday',()=>{
-    const date =new Date('02-28-2023');
+    const date =new Date('01-01-2023');
 
     const result =DateManager.isAHoliday(date)
 
